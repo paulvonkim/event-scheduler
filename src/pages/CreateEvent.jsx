1e0738ccbde5548afd3c8fe52fe1e1783e4ccdbd
Paul Kim <ppaauullkkiimm@gmail.com>
2025-01-10 11:49:04 +0100

    create event

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const CreateEvent = () => {
-  console.log("CreateEvent component is rendering");
+  // console.log("CreateEvent component is rendering");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [date, setDate] = useState("");
